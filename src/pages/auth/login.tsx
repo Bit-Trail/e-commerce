e4cd66f19bc4bb2736afae115ea4ff2cc617df06
AkshayK-03 <akshaysrivathsa600@gmail.com>
2025-03-02 22:07:34 +0530

    shop smart fe 1

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,8 +1,13 @@
+import { LoginForm } from '@/components/ui/login-form'
 import React from 'react'
 
 const login = () => {
   return (
-    <div>login</div>
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <LoginForm />
+      </div>
+    </div>
   )
 }
 
--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,8 +1,13 @@
+import { SignupFormDemo } from '@/components/ui/signup-form'
 import React from 'react'
 
 const register = () => {
   return (
-    <div>register</div>
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <SignupFormDemo />
+      </div>
+    </div>
   )
 }
 
